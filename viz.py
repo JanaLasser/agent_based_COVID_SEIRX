f6e27dd39c412b107d8308b180381d99b4967cff
Jana <jana.lasser@ds.mpg.de>
2020-10-06 17:02:22 +0200

    added indicator for quarantine

--- viz.py
+++ viz.py
@@ -5,20 +5,24 @@ import numpy as np
 colors = {'susceptible':'g',
 		  'exposed':'orange', 
 		  'infected':'red',
-	      'recovered':'gray'}
+	      'recovered':'gray',
+	      'quarantined':'blue',
+	      'testable':'k'}
 
-def draw_states(model, step, ax):
+
+def draw_states(model, step, pos, ax):
 	## draw patients
 	patients = [a.unique_id for a in model.schedule.agents if a.type == 'patient']
 
 	patient_states = model.datacollector.get_agent_vars_dataframe()
 	patient_states = patient_states.iloc[patient_states.index.isin(patients, level=1)] 
 
-	patient_states['color'] = patient_states['state'].replace(colors)
+	patient_states['color'] = patient_states['infection_state'].replace(colors)
 	color_list = patient_states.loc[step].sort_index()['color']
 
+	quarantine_states = patient_states.loc[step].sort_index()['quarantine_state']
+
 	G = model.G
-	pos = nx.drawing.layout.spring_layout(G, dim=2, weight='weight')
 	nodes = list(G.nodes)
 	nodes.sort()
 	
@@ -28,15 +32,22 @@ def draw_states(model, step, ax):
 			color='k', linewidth=weight, zorder=1)
 
 	for n in nodes:
-	    ax.scatter(pos[n][0], pos[n][1], color=color_list[n], s=50, zorder=2)
+		if quarantine_states[n]:
+			ax.scatter(pos[n][0], pos[n][1], color=color_list[n], s=50, zorder=2,\
+	    	edgecolors='k', linewidths=2)
+		else:
+			#print('not quarantined')
+			ax.scatter(pos[n][0], pos[n][1], color=color_list[n], s=50, zorder=2)
 
 	## draw employees
 	employees = [a.unique_id for a in model.schedule.agents if a.type == 'employee']
 	employee_states = model.datacollector.get_agent_vars_dataframe()
 	employee_states = employee_states.iloc[employee_states.index.isin(employees, level=1)] 
 
-	employee_states['color'] = employee_states['state'].replace(colors)
+	employee_states['color'] = employee_states['infection_state'].replace(colors)
 	color_list = employee_states.loc[step].sort_index()['color']
+
+	quarantine_states = employee_states.loc[step].sort_index()['quarantine_state']
 
 	N_employee = model.num_employees
 	ncol = 10
@@ -45,12 +56,14 @@ def draw_states(model, step, ax):
 	y_start = np.asarray([a[1] for a in pos.values()]).max()
 	y_stop = np.asarray([a[1] for a in pos.values()]).min()
 	y_step = (y_start - y_stop)/ncol
-	xpos = []
-	ypos = []
+
 	for i, e in enumerate(employees):
-		xpos.append(x_start + (int(i/ncol) + 1) * x_step)
-		ypos.append(y_start - i%ncol * y_step)
-	ax.scatter(xpos, ypos, color=color_list)
+		xpos = x_start + (int(i/ncol) + 1) * x_step
+		ypos = y_start - i%ncol * y_step
+		if quarantine_states[e]:
+			ax.scatter(xpos, ypos, color=color_list[e], edgecolors='k', linewidths=2)
+		else:
+			ax.scatter(xpos, ypos, color=color_list[e])
 
 	ax.text(x_start, y_stop - 0.2, 'employees')
 	ax.text(-0.2, y_stop - 0.2, 'patients')
@@ -81,6 +94,10 @@ def draw_infection_timeline(model, agent_type, ax):
 		 label='I', color=colors['infected'])
 	ax.plot(pop_numbers['R_{}'.format(agent_type)]/N, \
 		 label='R', color=colors['recovered'])
+	ax.plot(pop_numbers['X_{}'.format(agent_type)]/N, \
+		 label='X', color=colors['quarantined'])
+	ax.plot(pop_numbers['T_{}'.format(agent_type)]/N, '--',\
+		 label='T', color=colors['testable'])
 	ax.legend()
 	ax.set_xlabel('steps')
 	ax.set_ylabel('pdf')
