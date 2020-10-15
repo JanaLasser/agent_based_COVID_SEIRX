5ccb800e79fdee902da651cc492eaeb3e90b0568
Jana <jana.lasser@ds.mpg.de>
2020-10-16 00:03:32 +0200

    added differentiation between patient and employee screens and code for preventive screening. Fixed bug that caused newly positive employees not to enter quarantine

--- model_SEIRX.py
+++ model_SEIRX.py
@@ -53,8 +53,10 @@ def count_X_employee(model):
 def count_T_employee(model):
     T = np.asarray([a.testable for a in model.schedule.agents if a.type == 'employee']).sum()
     return T
-def check_screen(model):
-    return model.screened
+def check_patient_screen(model):
+    return model.screened_patients
+def check_employee_screen(model):
+    return model.screened_employees
 def get_infection_state(agent):
     if agent.exposed == True: return 'exposed'
     elif agent.infected == True: return 'infected'
@@ -70,8 +72,10 @@ class SIR(Model):
     G: interaction graph between agents
     verbosity: verbosity level [0, 1, 2]
     '''
-    def __init__(self, G, num_employees, verbosity, testing=True, 
-        time_until_test_result=2, index_probability=0.01):
+    def __init__(self, G, num_employees, verbosity=0, testing=True,
+        time_until_test_result=2, index_probability=0.01, follow_up_interval=4,
+        screening_interval_patients=3, screening_interval_employees=3, seed=0):
+
         self.verbosity = verbosity
         self.testing = testing # flag to turn off the testing strategy
         self.running = True # needed for the batch runner
@@ -105,9 +109,8 @@ class SIR(Model):
         self.subclinical_modifier = 0.5
 
         # testing strategy
-        self.testing_interval = 3 # days
-        self.Testing = Testing(self, self.testing_interval,
-                     self.verbosity)
+        self.Testing = Testing(self, follow_up_interval, screening_interval_patients,
+                     screening_interval_employees, verbosity)
 
         ## agents and their interactions
         self.G = G # interaction graph of patients
@@ -127,8 +130,10 @@ class SIR(Model):
             e = Employee(i, self, verbosity)
             self.schedule.add(e)
 
-        # flag that indicates whether a screen took place this turn
-        self.screened = False
+        # flag that indicates whether a screen took place this turn in a given
+        # agent group
+        self.screened_patients = False
+        self.screened_employees = False
 
         # list of agents that were tested positive this turn
         self.newly_positive_agents = []
@@ -136,7 +141,8 @@ class SIR(Model):
         self.scheduled_follow_up_screen = False
 
         # counter for days since the last test screen
-        self.days_since_last_screen = None
+        self.days_since_last_patient_screen = 0
+        self.days_since_last_employee_screen = 0
         
         # data collectors to save population counts and patient / employee
         # states every time step
@@ -153,31 +159,43 @@ class SIR(Model):
                                'R_employee':count_R_employee,
                                'X_employee':count_X_employee,
                                'T_employee':count_T_employee,
-                               'screen':check_screen},
+                               'screen_patients':check_patient_screen,
+                               'screen_employees':check_employee_screen},
             agent_reporters = {'infection_state':get_infection_state,
                                'quarantine_state':get_quarantine_state})
 
-    def test_agents(self):
-        untested_agents = [a for a in self.schedule.agents if a.tested == False]
+    def test_agents(self, agent_group):
+        untested_agents = [a for a in self.schedule.agents if \
+            (a.tested == False and a.type == agent_group)]
+
         if len(untested_agents) > 0:
-            self.screen = True
-            for a in self.schedule.agents:
-                if a.tested == False:
-                    a.tested = True
-                    if a.testable == True:
-                        if self.verbosity > 0: print('{} {} sent positive sample'.format(a.type, a.ID))
-                        a.sample = 'positive'
-                    else:
-                        a.sample = 'negative'
+            if agent_group == 'patient': 
+                self.screened_patients = True
+            elif agent_group == 'employee': 
+                self.screened_employees = True
+            else:
+                print('unknown agent group!')
+            
+            for a in untested_agents:
+                a.tested = True
+                if a.testable == True:
+                    if self.verbosity > 0: print('{} {} sent positive sample'\
+                        .format(a.type, a.ID))
+                    a.sample = 'positive'
+                else:
+                    a.sample = 'negative'
         
     def step(self):
+        if self.verbosity > 1: 
+            print('days since last screen: {}'.format(self.days_since_last_screen))
         if self.testing:
             # act on new test results
             if len(self.newly_positive_agents) > 0:
-                if self.verbosity > 0: print('new positive test')
+                if self.verbosity > 0: print('new positive test(s)')
                 # send all K1 contacts of positive agents into quarantine
                 # patients. NOTE: so far this is only implemented for patients
                 for a in self.newly_positive_agents:
+                    a.quarantined = True
                     if a.type == 'patient':
                         # find all agents that share edges with the given agent
                         # that are classified as K1 contact areas in the testing
@@ -203,26 +221,60 @@ class SIR(Model):
             # Testing.interval steps ago or
             # (b) as a follow-up screen for a screen that was initiated becuase of
             # new positive cases
-            if (self.days_since_last_screen == None or \
-                self.days_since_last_screen >= self.Testing.interval):
-                # (a)
-                if self.new_positive_tests == True:
-                    if self.verbosity > 0: print('initiating screen because of positive test(s)')
-                    self.test_agents()
-                    self.screened = True
-                    self.days_since_last_screen = 0
-                    self.scheduled_follow_up_screen = True
-                # (b)
-                elif self.scheduled_follow_up_screen == True:
-                    if self.verbosity > 0: print('initiating follow-up screen')
-                    self.test_agents()
-                    self.screened = True
-                    self.days_since_last_screen = 0
-                    self.scheduled_follow_up_screen = False 
+            # (c) if there is a preventive screening policy and it is time for
+            # a preventive screen in a given agent group
+
+            # (a)
+            if self.new_positive_tests == True:
+                if self.verbosity > 0: print('initiating screen because of positive test(s)')
+                self.test_agents('patient')
+                self.screened_patients = True
+                self.days_since_last_patient_screen = 0
+                self.test_agents('employee')
+                self.screening_employees = True
+                self.days_since_last_employee_screen = 0
+                
+                self.scheduled_follow_up_screen = True
+            # (b)
+            elif self.scheduled_follow_up_screen == True \
+                and self.days_since_last_patient_screen > self.Testing.follow_up_interval:
+                if self.verbosity > 0: print('initiating follow-up screen')
+                self.test_agents('patient')
+                self.screened_patients = True
+                self.days_since_last_patient_screen = 0
+                self.test_agents('employee')
+                self.screened_employees = True
+                self.days_since_last_employee_screen = 0
+                self.scheduled_follow_up_screen = False 
+            # (c) 
+            elif (self.Testing.screening_interval_patients != None or\
+                  self.Testing.screening_interval_employees != None):
+                # preventive patient screens
+                if self.Testing.screening_interval_patients != None and\
+                   self.days_since_last_patient_screen > self.Testing.screening_interval_patients:
+                    if self.verbosity > 0: print('initiating preventive patient screen')
+                    self.test_agents('patient')
+                    self.screened_patients = True
+                    self.days_since_last_patient_screen = 0
+                else:
+                    self.screened_patients = False
+                    self.days_since_last_patient_screen += 1
+
+                # preventive employee screens
+                if self.Testing.screening_interval_employees != None and\
+                   self.days_since_last_employee_screen > self.Testing.screening_interval_employees:
+                   if self.verbosity > 0: print('initiating preventive employee screen')
+                   self.test_agents('employees')
+                   self.screened_employees = True
+                   self.days_since_last_employee_screen = 0 
+                else:
+                    self.screened_employees = False
+                    self.days_since_last_employee_screen += 1
             else:
-                self.screened = False
-                if self.days_since_last_screen != None:
-                    self.days_since_last_screen += 1
+                self.screened_patients = False
+                self.screened_employees = False
+                self.days_since_last_patient_screen += 1
+                self.days_since_last_employee_screen += 1
 
 
             # find symptomatic agents that have not been tested yet and are not in
@@ -239,12 +291,6 @@ class SIR(Model):
                 else:
                     a.sample = 'negative'
 
-        # launches an employee screen every testing_interval step
-        #if self.Nstep % self.testing_interval == 0:
-        #    cases = self.Testing.screen('employee')
-            # if infected employees are detected, an patient screen is launched
-        #    if cases > 0:
-        #        _ = self.Testing.screen('patient')
 
         self.schedule.step()
         self.datacollector.collect(self)
