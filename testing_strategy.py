ee1c0d2074b2e94a6ad8d93f3f82092d21af34b8
Jana <jana.lasser@ds.mpg.de>
2020-10-22 18:30:34 +0200

    renamed variable

--- testing_strategy.py
+++ testing_strategy.py
@@ -1,7 +1,7 @@
 class Testing():
-	def __init__(self, model, follow_up_interval, screening_interval_patients, 
+	def __init__(self, model, follow_up_testing_interval, screening_interval_patients, 
 		screening_interval_employees, verbosity):
-		self.follow_up_interval = follow_up_interval
+		self.follow_up_testing_interval = follow_up_testing_interval
 		self.screening_interval_patients = screening_interval_patients
 		self.screening_interval_employees = screening_interval_employees
 		self.model = model
