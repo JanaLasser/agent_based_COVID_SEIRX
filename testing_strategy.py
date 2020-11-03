7585d53c78204bce9071cce18ef706f4f54db90c
Jana <jana.lasser@ds.mpg.de>
2020-11-03 10:38:24 +0100

    modified pre-infectiousness detection of PCR

--- testing_strategy.py
+++ testing_strategy.py
@@ -47,7 +47,7 @@ class Testing():
 	     {
 	         'sensitivity':1,
 	         'specificity':1,
-	         'time_until_testable':3,
+	         'time_until_testable':4,
 	         'time_testable':model.infection_duration,
 	         'time_until_test_result':0
 	     },
@@ -55,7 +55,7 @@ class Testing():
 	     {
 	         'sensitivity':1,
 	         'specificity':1,
-	         'time_until_testable':3,
+	         'time_until_testable':4,
 	         'time_testable':model.infection_duration,
 	         'time_until_test_result':1
 	     },
@@ -63,7 +63,7 @@ class Testing():
 	     {
 	         'sensitivity':1,
 	         'specificity':1,
-	         'time_until_testable':3,
+	         'time_until_testable':4,
 	         'time_testable':model.infection_duration,
 	         'time_until_test_result':2
 	     },
