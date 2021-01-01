252833c6cab23cab2b97db7110d4043e6a97872e
Jana <jana.lasser@ds.mpg.de>
2021-01-01 23:40:48 +0100

    test scripts for cluster output and errors

--- school/test_cluster_output.sh
+++ school/test_cluster_output.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH -J COVID_SEIRX_calibration              ## name
+#SBATCH -J output_error_test              
 #SBATCH -N 1                 
 #SBATCH -o output
 #SBATCH -e error
-#SBATCH --ntasks=32          ## number of tasks per node
+#SBATCH --ntasks=16          
 #SBATCH --time=00:00:05      
 #SBATCH --mail-type=BEGIN,END
 #SBATCH --mail-user=lasser@csh.ac.at
