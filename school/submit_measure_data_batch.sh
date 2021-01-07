68beae70b38f72f71107a48c5a46021c207e6ada
Jana <jana.lasser@ds.mpg.de>
2021-01-07 15:22:17 +0100

    batch submission script for prevention measure data creation on marvin

---
+++ school/submit_measure_data_batch.sh
@@ -0,0 +1,17 @@
+N_runs=300
+measure_step=64
+school_type=primary
+
+for school_layout_start_index in $(seq 0 14)
+   do
+   school_layout_end_index=`echo $school_layout_start_index+1 | bc`
+   
+   for measure_start_index in $(seq 0 $measure_step 127)
+      do
+      measure_end_index=`echo $measure_start_index+$measure_step | bc`
+
+      echo python3 run_data_creation.py $school_type $N_runs $school_layout_start_index $school_layout_end_index $measure_start_index $measure_end_index &
+
+   done
+
+done
