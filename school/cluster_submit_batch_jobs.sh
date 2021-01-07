0a1cf9c42cdd403fce61e49cbc7a26ed2e8561df
Jana <jana.lasser@ds.mpg.de>
2021-01-07 17:19:04 +0100

    renamed

--- school/cluster_submit_batch_jobs.sh
+++ school/cluster_submit_batch_jobs.sh
@@ -1,41 +1,34 @@
 #!/bin/bash
-#SBATCH -J COVID_SEIRX_calibration_primary
-#SBATCH -N 4                 
+#SBATCH -J COVID_SEIRX_data_creation_primary_dc
+#SBATCH -N 2                 
 #SBATCH -o output
 #SBATCH -e error
 #SBATCH --ntasks-per-core=2
 #SBATCH --ntasks=32          
-#SBATCH --time=01:00:00      
+#SBATCH --time=0      
 #SBATCH --mail-type=BEGIN,END
 #SBATCH --mail-user=lasser@csh.ac.at
 
 module purge
-module load python/3.7.4
-module load numpy/1.15.4
-module load scipy/1.3.1
+module module load anaconda3/5.3.0
+source /opt/sw/x86_64/glibc-2.17/ivybridge-ep/anaconda3/5.3.0/etc/profile.d/conda.sh
+conda activate covid
 
-N_runs=2000
 
-for school_type in primary
+N_runs=300
+measure_step=64
+school_type=primary
+
+for school_layout_start_index in $(seq 0 14)
    do
+   school_layout_end_index=`echo $school_layout_start_index+1 | bc`
    
-   for icw in 0.3 0.35 0.4 0.45
+   for measure_start_index in $(seq 0 $measure_step 127)
       do
-
-      for fcw in 0.3 0.35 0.4 0.45
-      do
-
-         for atd in -0.04 -0.035 -0.03 -0.025
-            do
-
-            python3 run_calibration_cluster.py $school_type $N_runs $icw $fcw $atd &
-            hostname &
-
-
-
-         done
-
-      done
+      measure_end_index=`echo $measure_start_index+$measure_step | bc`
+      echo python3 run_data_creation.py $school_type $N_runs $school_layout_start_index $school_layout_end_index $measure_start_index $measure_end_index 
+      echo hostname
+      python3 run_data_creation.py $school_type $N_runs $school_layout_start_index $school_layout_end_index $measure_start_index $measure_end_index &
 
    done
 
