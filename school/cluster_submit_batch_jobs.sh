36f11fa5694ad4c32f3b47d1c3b4138ce21ee8c9
Jana <jana.lasser@ds.mpg.de>
2021-01-08 13:12:24 +0100

    new stdout and stderr piping

--- school/cluster_submit_batch_jobs.sh
+++ school/cluster_submit_batch_jobs.sh
@@ -26,10 +26,11 @@ for school_layout_start_index in $(seq 0 9)
    
    for measure_start_index in $(seq 0 $measure_step 4)
       do
+      fname=$school_type-$N_runs-$school_layout_start_index-$school_layout_end_index-$measure_start_index-$measure_end_index
       measure_end_index=`echo $measure_start_index+$measure_step | bc`
       echo python run_data_creation.py $school_type $N_runs $school_layout_start_index $school_layout_end_index $measure_start_index $measure_end_index 
       echo $HOSTNAME
-      python run_data_creation.py $school_type $N_runs $school_layout_start_index $school_layout_end_index $measure_start_index $measure_end_index &
+      python run_data_creation.py $school_type $N_runs $school_layout_start_index $school_layout_end_index $measure_start_index $measure_end_index > stdout-$fname 2> stderr-$fname &
 
    done
 
