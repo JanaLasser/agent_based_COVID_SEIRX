dc223ad3a0a9d1e51cca8fa20a1276754caab1e8
Jana <jana.lasser@ds.mpg.de>
2021-01-08 09:45:12 +0100

    testing settings

--- school/cluster_submit_batch_jobs.sh
+++ school/cluster_submit_batch_jobs.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #
 #SBATCH -J COVID_SEIRX_data_creation_primary_dc
-#SBATCH -N 2                 
+#SBATCH -N 1                 
 #SBATCH -o output
 #SBATCH -e error
 #SBATCH --ntasks-per-core=2
 #SBATCH --ntasks=32          
-#SBATCH --time=36:00:00      
+#SBATCH --time=00:05:00      
 #SBATCH --mail-type=BEGIN,END
 #SBATCH --mail-user=lasser@csh.ac.at
 
@@ -16,15 +16,15 @@ source /opt/sw/x86_64/glibc-2.17/ivybridge-ep/anaconda3/5.3.0/etc/profile.d/cond
 conda activate covid
 
 
-N_runs=300
-measure_step=64
+N_runs=5
+measure_step=1
 school_type=primary_dc
 
-for school_layout_start_index in $(seq 0 14)
+for school_layout_start_index in $(seq 0 9)
    do
    school_layout_end_index=`echo $school_layout_start_index+1 | bc`
    
-   for measure_start_index in $(seq 0 $measure_step 127)
+   for measure_start_index in $(seq 0 $measure_step 2)
       do
       measure_end_index=`echo $measure_start_index+$measure_step | bc`
       echo python3 run_data_creation.py $school_type $N_runs $school_layout_start_index $school_layout_end_index $measure_start_index $measure_end_index 
