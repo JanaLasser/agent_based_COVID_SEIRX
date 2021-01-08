cd2a541331b821658bc950d42e8d5c47cdad854c
Jana <jana.lasser@ds.mpg.de>
2021-01-08 16:07:47 +0100

    working setup

--- school/cluster_submit_batch_jobs.sh
+++ school/cluster_submit_batch_jobs.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 #
 #SBATCH -J COVID_SEIRX_data_creation_primary_dc
-#SBATCH -N 2                 
-#SBATCH -o output
-#SBATCH -e error
+#SBATCH -N 1                 
+#SBATCH -o output_primary_dc
+#SBATCH -e error_primary_dc
 #SBATCH --ntasks-per-core=2
 #SBATCH --ntasks=16          
-#SBATCH --time=00:05:00      
+#SBATCH --time=36:00:00      
 #SBATCH --mail-type=BEGIN,END
 #SBATCH --mail-user=lasser@csh.ac.at
 
+uptime
 echo -n "start: "
 date
 
@@ -20,21 +21,20 @@ conda deactivate
 conda activate covid
 
 
-N_runs=20
-measure_step=2
+N_runs=300
+measure_step=64
 school_type=primary_dc
 
 for school_layout_start_index in $(seq 0 14)
    do
    school_layout_end_index=`echo $school_layout_start_index+1 | bc`
    
-   for measure_start_index in $(seq 0 $measure_step 4)
+   for measure_start_index in $(seq 0 $measure_step 127)
       do
-      fname=$school_type-$N_runs-$school_layout_start_index-$school_layout_end_index-$measure_start_index-$measure_end_index
       measure_end_index=`echo $measure_start_index+$measure_step | bc`
       echo python run_data_creation.py $school_type $N_runs $school_layout_start_index $school_layout_end_index $measure_start_index $measure_end_index 
       echo $HOSTNAME
-      python run_data_creation.py $school_type $N_runs $school_layout_start_index $school_layout_end_index $measure_start_index $measure_end_index &
+      #python run_data_creation.py $school_type $N_runs $school_layout_start_index $school_layout_end_index $measure_start_index $measure_end_index &
    done
 
 done
@@ -42,3 +42,4 @@ done
 wait 
 echo -n "end: "
 date
+uptime
