ce56940311b3c26be91fb26e4965c61648bf585d
Jana <jana.lasser@ds.mpg.de>
2021-01-11 21:54:11 +0100

    changed to upper secondary

--- school/cluster_submit_batch_jobs_test.sh
+++ school/cluster_submit_batch_jobs_test.sh
@@ -15,7 +15,7 @@
 uptime
 echo -n "start: "
 date
-echo "lower_secondary test"
+echo "upper_secondary test"
 
 module purge
 module load anaconda3/5.3.0
@@ -25,7 +25,7 @@ conda activate covid
 
 
 N_runs=1
-school_type=lower_secondary             
+school_type=upper_secondary             
 max_tasks=32                 ## number of tasks per node.
 running_tasks=0              ## initialization
 
