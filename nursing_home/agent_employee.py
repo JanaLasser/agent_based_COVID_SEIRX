fa79c1d75f3c3df1631f0eca3b7e7eca9fe3d61d
Jana <jana.lasser@ds.mpg.de>
2021-01-14 19:58:56 +0100

    updated code to work with new features from school

--- nursing_home/agent_employee.py
+++ nursing_home/agent_employee.py
@@ -20,57 +20,30 @@ class employee(agent_SEIRX):
 
     def step(self):
         '''
-        Infection step: if an employee is infected and not in quarantine, it 
-        iterates through all other residents and employees tries to 
-        infect them. Infections are staged here and only applied in the 
+        Infection step: if a teacher is infected and not in quarantine, it 
+        interacts with other students and teachers trough the specified 
+        contact network and can pass a potential infection.
+        Infections are staged here and only applied in the 
         "advance"-step to simulate "simultaneous" interaction
         '''
+
         # check for external infection in continuous index case modes
         if self.model.index_case in ['continuous'] and \
            self.index_probability > 0:
-	        self.introduce_external_infection()
+            self.introduce_external_infection()
 
-        # simulate contacts to other employees and residents if the agent is
+        # simulate contacts to other agents if the agent is
         # infected and not in quarantine. Randomly transmit the infection 
         # according to the transmission risk
         if self.infectious:
             if not self.quarantined:
-                # infectiousness is constant and high during the first 2 days 
-                # (pre-symptomatic) and then decreases monotonically until agents 
-                # are not infectious anymore at the end of the infection_duration 
-                modifier = 1 - max(0, self.days_since_exposure - self.exposure_duration - 1) / \
-                    (self.infection_duration - self.exposure_duration - 1)
 
-                # if infectiousness is modified for asymptomatic cases, multiply
-                # the asymptomatic modifier with the days-infected modifier 
-                if self.symptomatic_course == False:
-                    modifier *= self.model.subclinical_modifier
+                # get contacts to other agent groups according to the
+                # interaction network
+                employees = self.get_contacts('employee')
+                residents = self.get_contacts('resident')
 
-                # get employee and resident contacts according to contact rules
-                # and the interaction network
-                residents = self.get_employee_resident_contacts()
-                employees = self.get_employee_employee_contacts()
-
-                # code transmission to residents and transmission to employees
+                # code transmission to other agent groups
                 # separately to allow for differences in transmission risk
-                self.transmit_infection(residents, 
-                    self.transmission_risk, modifier)
-                self.transmit_infection(employees, 
-                    self.transmission_risk, modifier)
-
-
-    def get_employee_resident_contacts(self):
-        # only contacts to residents in the same unit are possible
-        contacts = [a for a in self.model.schedule.agents if
-            (a.type == 'resident' and a.unit == self.unit)]
-        return contacts
-
-    def get_employee_employee_contacts(self):
-        # only contacts to employees in the same unit
-        contacts = [a for a in self.model.schedule.agents if
-            (a.type == 'employee' and a.unit == self.unit)]
-
-        # TODO: implement random cross-unit interaction of employees
-        # if self.model.employee_cross_unit_interaction:
-        return contacts
-
+                self.transmit_infection(employees)
+                self.transmit_infection(residents)
