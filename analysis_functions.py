c4491b1f409613c6d0122582b6d7ceeb81972c7d
Jana <jana.lasser@ds.mpg.de>
2021-03-09 14:48:24 +0100

    added functionality to analyse parameters that are varied within the scope of the sensitivity analysis

--- analysis_functions.py
+++ analysis_functions.py
@@ -7,6 +7,8 @@ import pickle
 import bz2
 import _pickle as cPickle
 from os.path import join
+from random import shuffle
+import time
 
 import sys
 sys.path.insert(0,'school')
@@ -376,6 +378,18 @@ def get_ensemble_observables_school(model, run):
     quarantine_days_student = model.quarantine_counters['student']
     quarantine_days_teacher = model.quarantine_counters['teacher']
     quarantine_days_family_member = model.quarantine_counters['family_member']
+    diagnostic_test_detected_infections_student = \
+        data['diagnostic_test_detected_infections_student'].max()
+    diagnostic_test_detected_infections_teacher = \
+        data['diagnostic_test_detected_infections_teacher'].max()
+    diagnostic_test_detected_infections_family_member = \
+        data['diagnostic_test_detected_infections_family_member'].max()
+    preventive_test_detected_infections_student = \
+        data['preventive_test_detected_infections_student'].max()
+    preventive_test_detected_infections_teacher = \
+        data['preventive_test_detected_infections_teacher'].max()
+    preventive_test_detected_infections_family_member = \
+        data['preventive_test_detected_infections_family_member'].max()
     pending_test_infections = data['pending_test_infections'].max()
     undetected_infections = data['undetected_infections'].max()
     predetected_infections = data['predetected_infections'].max()
@@ -405,6 +419,18 @@ def get_ensemble_observables_school(model, run):
           'quarantine_days_student':quarantine_days_student,
           'quarantine_days_teacher':quarantine_days_teacher,
           'quarantine_days_family_member':quarantine_days_family_member,
+          'preventive_test_detected_infections_student':\
+                preventive_test_detected_infections_student,
+          'preventive_test_detected_infections_teacher':\
+                preventive_test_detected_infections_teacher,
+          'preventive_test_detected_infections_family_member':\
+                preventive_test_detected_infections_family_member,
+          'diagnostic_test_detected_infections_student':\
+                diagnostic_test_detected_infections_student,
+          'diagnostic_test_detected_infections_teacher':\
+                diagnostic_test_detected_infections_teacher,
+          'diagnostic_test_detected_infections_family_member':\
+                diagnostic_test_detected_infections_family_member,
           'pending_test_infections':pending_test_infections,
           'undetected_infections':undetected_infections,
           'predetected_infections':predetected_infections,
@@ -417,8 +443,16 @@ def get_ensemble_observables_school(model, run):
 
 
 def compress_pickle(fname, fpath, data):
-    with bz2.BZ2File(join(fpath, fname + '.pbz2'), 'w') as f: 
-        cPickle.dump(data, f)
+    success = False
+    while not success:
+        try:
+            with bz2.BZ2File(join(fpath, fname + '.pbz2'), 'w') as f: 
+                cPickle.dump(data, f)
+                success = True
+        except OSError:
+            time.sleep(0.5)
+            print('re-trying to dump model file {} ...'.format(fname))
+    return
     
     
 def decompress_pickle(fname, fpath):
@@ -429,6 +463,7 @@ def decompress_pickle(fname, fpath):
 
 def get_representative_run(N_infected, path):
     filenames = os.listdir(path)
+    shuffle(filenames)
     medians = {int(f.split('_')[1]):int(f.split('_')[3].split('.')[0]) \
                for f in filenames}
     dist = np.inf
@@ -451,7 +486,11 @@ def dump_JSON(path, school,
               screen_frequency_teacher, teacher_mask, student_mask, half_classes,
               ventilation_mod, node_list, teacher_schedule, student_schedule, 
               rep_transmission_events, state_data, start_weekday, duration,
-              fname_addition=''):
+              fname_addition='', friendship_contacts=False,
+              class_size_reduction=False,
+              m_efficiency_exhale=False, m_efficiency_inhale=False,
+              s_test_rate=False, t_test_rate=False,
+              trisk_mod = False):
 
     student_schedule = student_schedule.reset_index()
     teacher_schedule = teacher_schedule.reset_index()
@@ -503,14 +542,195 @@ def dump_JSON(path, school,
             'start_weekday':start_weekday,
             'duration':duration}
 
+
     fname = join(path, 'test-{}_'.format(ttype) + \
        'turnover-{}_index-{}_tf-{}_'
        .format(turnover, index_case[0], screen_frequency_teacher) +\
-       'sf-{}_tmask-{}_smask-{}_half-{}_vent-{}'\
+       'sf-{}_tmask-{}_smask-{}'\
        .format(screen_frequency_student, bool_dict[teacher_mask],\
-        bool_dict[student_mask], bool_dict[half_classes], ventilation_mod))
+        bool_dict[student_mask]))
+
+
+    if friendship_contacts:
+        fname = fname + '_fcontacts-{}'.format(friendship_contacts)
+    if class_size_reduction:
+        fname = fname + '_csizered-{}'.format(class_size_reduction)
+    if m_efficiency_exhale and m_efficiency_inhale:
+        fname = fname + '_meffinh-{}_meffexh-{}'\
+            .format(m_efficiency_exhale, m_efficiency_inhale)
+    if s_test_rate and t_test_rate:
+        fname = fname + '_stestrate-{}_ttestrate-{}'\
+        .format(s_test_rate, t_test_rate)
+    if trisk_mod:
+        fname = fname + '_trisk-{}'.format(trisk_mod)
+
+    fname = fname + '_half-{}_vent-{}'\
+        .format(bool_dict[half_classes], ventilation_mod)
     fname = fname + fname_addition + '.txt'
 
     with open(fname,'w')\
                    as outfile:
         json.dump(data, outfile)
+
+def get_measures(measure_string, test_participation_rate=False,
+        reduced_class_size=False, added_friendship_contacts=False,
+        reduced_mask_efficiency=False, transmission_risk_modifier=False):
+    '''
+    Convenience function to get the individual measures given a string (filename)
+    of measures
+    '''
+    #print(measure_string)
+    agents = {
+        'student':{
+                'screening_interval': None, 
+                'index_probability': 0, 
+                'mask':False},
+        'teacher':{
+                'screening_interval': None, 
+                'index_probability': 0, 
+                'mask':False},
+        'family_member':{
+                'screening_interval': None, 
+                'index_probability': 0, 
+                'mask':False} 
+}
+    
+    turnovers = {0:'same', 1:'one', 2:'two', 3:'three'}
+    bmap = {'T':True, 'F':False}
+    interval_map = {'0':0, '3':3, '7':7, '14':14, 'None':None}
+    index_map = {'s':'student', 't':'teacher'}
+    
+    stype, _ = measure_string.split('_test')
+    rest = measure_string.split(stype + '_')[1]
+
+    if test_participation_rate and reduced_class_size and \
+       reduced_mask_efficiency and transmission_risk_modifier:
+        ttpype, turnover, index, tf, sf, tmask, smask, \
+        class_size_reduction, vent, meffexh, meffinh, \
+        s_test_rate, t_test_rate, trisk = rest.split('_')
+        tmp = [stype, ttpype, turnover, index, tf, sf, tmask,
+               smask, class_size_reduction, vent, meffexh, meffinh,
+               s_test_rate, t_test_rate, trisk]
+        
+    elif test_participation_rate:
+        ttpype, turnover, index, tf, sf, tmask, smask, haf, \
+            s_test_rate, t_test_rate, vent = rest.split('_')
+        tmp = [stype, ttpype, turnover, index, tf, sf, tmask,
+         smask, haf, s_test_rate, t_test_rate, vent]
+    elif reduced_class_size:
+        ttpype, turnover, index, tf, sf, tmask, smask, haf, \
+            class_size_reduction, vent = rest.split('_')
+        tmp = [stype, ttpype, turnover, index, tf, sf, tmask,
+         smask, haf, class_size_reduction, vent]
+    elif added_friendship_contacts:
+        ttpype, turnover, index, tf, sf, tmask, smask, haf, \
+            friendship_contacts, vent = rest.split('_')
+        tmp = [stype, ttpype, turnover, index, tf, sf,
+            tmask, smask, haf, friendship_contacts, vent]
+    elif reduced_mask_efficiency:
+        ttpype, turnover, index, tf, sf, tmask, smask, \
+        meffexh, meffinh, haf, vent = rest.split('_')
+        tmp = [stype, ttpype, turnover, index, tf, sf,
+            tmask, smask, haf, meffexh, meffinh, vent]
+    else:
+        ttpype, turnover, index, tf, sf, tmask, smask, haf, vent = \
+            rest.split('_')
+        tmp = [stype, ttpype, turnover, index, tf, sf, tmask, smask, haf, vent]
+
+    tmp = [m.split('-') for m in tmp]
+    screening_params = {}
+    
+    half = False
+    for m in tmp:
+        if len(m) == 1:
+            pass
+        elif m[0] == 'test':
+            ttype = '{}_day_{}'.format(turnovers[int(tmp[2][1])], tmp[1][1])
+            screening_params['preventive_test_type'] = ttype
+        elif m[0] == 'turnover':
+            pass
+        elif m[0] == 'index':
+            screening_params['index_case'] = index_map[m[1]]
+        elif m[0] == 'tf':
+            agents['teacher']['screening_interval'] = interval_map[m[1]]
+        elif m[0] == 'sf':
+            agents['student']['screening_interval'] = interval_map[m[1]]
+        elif m[0] == 'tmask':
+            agents['teacher']['mask'] = bmap[m[1]]    
+        elif m[0] == 'smask':
+            agents['student']['mask'] = bmap[m[1]]
+        elif m[0] == 'half':
+            half = bmap[m[1]]
+        elif m[0] == 'vent':
+            screening_params['transmission_risk_ventilation_modifier'] = float(m[1])
+        elif m[0] == 'csizered':
+            screening_params['class_size_reduction'] = float(m[1])
+        elif m[0] == 'stestrate':
+            screening_params['student_test_rate'] = float(m[1])
+        elif m[0] == 'ttestrate':
+            screening_params['teacher_test_rate'] = float(m[1])
+        elif m[0] == 'fcontacts':
+            screening_params['added_friendship_contacts'] = float(m[1])
+        elif m[0] == 'meffexh':
+            screening_params['mask_efficiency_exhale'] = float(m[1])
+        elif m[0] == 'meffinh':
+            screening_params['mask_efficiency_inhale'] = float(m[1])
+        elif m[0] == 'trisk':
+            screening_params['transmission_risk_modifier'] = float(m[1])
+        else:
+            print('unknown measure type ', m[0])
+    
+    return screening_params, agents, half
+
+
+def get_data(stype, src_path, test_participation_rate=False,
+            reduced_class_size=False, added_friendship_contacts=False,
+            reduced_mask_efficiency=False, transmission_risk_modifier=False):
+    '''
+    Convenience function to read all ensembles from different measures
+    of a given school type and return one single data frame
+    '''
+    data = pd.DataFrame()
+    stype_path = join(src_path, stype)
+    files = os.listdir(stype_path)
+    for f in files:
+        screening_params, agents, half = get_measures(f.strip('.csv'),
+                test_participation_rate=test_participation_rate,
+                reduced_class_size=reduced_class_size, 
+                added_friendship_contacts=added_friendship_contacts,
+                reduced_mask_efficiency=reduced_mask_efficiency,
+                transmission_risk_modifier=transmission_risk_modifier)
+
+        ensmbl = pd.read_csv(join(stype_path, f))
+        try:
+            ensmbl = ensmbl.drop(columns=['Unnamed: 0'])
+        except KeyError:
+            pass
+        ensmbl['preventive_test_type'] = screening_params['preventive_test_type']
+        ensmbl['index_case'] = screening_params['index_case']
+        ensmbl['transmission_risk_ventilation_modifier'] = \
+            screening_params['transmission_risk_ventilation_modifier']
+        ensmbl['student_mask'] = agents['student']['mask']
+        ensmbl['teacher_mask'] = agents['teacher']['mask']
+        ensmbl['student_screening_interval'] = agents['student']['screening_interval']
+        ensmbl['teacher_screening_interval'] = agents['teacher']['screening_interval']
+        ensmbl['half_classes'] = half
+        if reduced_class_size:
+            ensmbl['class_size_reduction'] = screening_params['class_size_reduction']
+
+        if test_participation_rate:
+            ensmbl['student_test_rate'] = screening_params['student_test_rate']
+            ensmbl['teacher_test_rate'] = screening_params['teacher_test_rate']
+
+        if reduced_mask_efficiency:
+            ensmbl['mask_efficiency_inhale'] = screening_params['mask_efficiency_inhale']
+            ensmbl['mask_efficiency_exhale'] = screening_params['mask_efficiency_exhale']
+            
+        if transmission_risk_modifier:
+            ensmbl['transmission_risk_modifier'] = screening_params['transmission_risk_modifier']
+        data = pd.concat([data, ensmbl])
+
+    data = data.reset_index(drop=True)
+    data['teacher_screening_interval'] = data['teacher_screening_interval'].replace({None:'never'})
+    data['student_screening_interval'] = data['student_screening_interval'].replace({None:'never'})
+    return data
